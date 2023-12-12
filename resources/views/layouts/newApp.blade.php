b8d15ba717331f310c774049e69aa4f18cb4bb89
Rhysdhyde5 <2120303@swansea.ac.uk>
2023-12-12 21:46:23 +0000

    updated front end to look slightly better

--- resources/views/layouts/newApp.blade.php
+++ resources/views/layouts/newApp.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,4 +13,120 @@
     @yield('content')
 
 </body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'GymGo')</title>
+
+
+
+    <!-- Add your stylesheets or link to a CDN here -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- Add your custom styles -->
+    <style>body {
+        background-color: #f8f9fa;
+        color: #495057;
+    }
+
+    nav.navbar {
+        background-color: hsl(0, 0%, 100%);
+    }
+
+    nav.navbar a.navbar-brand,
+    nav.navbar a.nav-link,
+    form.navbar-nav button {
+        color: #394244;
+    }
+
+    nav.navbar a.navbar-brand:hover,
+    nav.navbar a.nav-link:hover,
+    form.navbar-nav button:hover {
+        color: #ffffff;
+    }
+
+    div.container {
+        background-color: #ffffff;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        margin-top: 20px;
+    }
+
+    .btn-primary {
+        background-color: #007bff;
+        border-color: #007bff;
+    }
+
+    .btn-primary:hover {
+        background-color: #0056b3;
+        border-color: #0056b3;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('dashboard') }}">GymGo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('posts.index') }}">Posts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('users.index') }}">Users</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- Right-aligned items, such as login/logout, user profile, etc. -->
+        @auth
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('profile.edit') }}">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit" class="btn btn-link nav-link">Logout</button>
+                    </form>
+                </li>
+            </ul>
+        @else
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+            </ul>
+        @endauth
+
+    </nav>
+
+    <div class="container mt-4">
+        {{-- Content Section --}}
+        @yield('content')
+    </div>
+
+    <!-- Add your scripts or link to a CDN here -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
